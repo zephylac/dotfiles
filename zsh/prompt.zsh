28bb255b96d1d4a083a88e84283b815b9e030b68
zephylac <zephylac@users.noreply.github.com>
2017-03-19 17:29:12 +0100

    Update prompt.zsh

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -124,8 +124,8 @@ precmd() {
 
   # show username@host if logged in through SSH
   [[ "$SSH_CONNECTION" != ''  ]] && IPA=$(wget -qO- http://ipecho.net/plain &)
-  [[ "$SSH_CONNECTION" != ''  ]] && prompt_username=" %F{orange}%n%f@%F{red}$IPA%f"
-  print -P '\n%$prompt_username%F{yellow}$(cmd_exec_time)%f'
+  [[ "$SSH_CONNECTION" != ''  ]] && prompt_username=" %n%f@%F{red}$IPA%f"
+  print -P '\n%F{orange}%$prompt_username%F{yellow}$(cmd_exec_time)%f'
 
   # remove the cmd_timestamp, indicating that precmd has completed
   unset cmd_timestamp
